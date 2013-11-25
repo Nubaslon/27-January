53546bbf058d7e4b035f5587edcff607e2e227df
Nubaslon <nubaslon@gmail.com>
2013-11-25 22:25:15 +0400

    Initial Commit

---
+++ 27 January/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  27 January
+//
+//  Created by Nubaslon on 25.11.13.
+//  Copyright (c) 2013 Nubaslon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ 27 January/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  27 January
+//
+//  Created by Nubaslon on 25.11.13.
+//  Copyright (c) 2013 Nubaslon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
